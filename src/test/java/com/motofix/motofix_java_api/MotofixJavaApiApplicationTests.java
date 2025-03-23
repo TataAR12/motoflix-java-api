896163f9be79f9b0ea3d3413a0a4f1a33c12b0db
TataAR12 <angierodri200@gmail.com>
2025-03-23 17:42:09 -0500

    first commit

---
+++ src/test/java/com/motofix/motofix_java_api/MotofixJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motofix.motofix_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotofixJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
